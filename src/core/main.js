6d54745cc31ec4791d5d140a9d37ec35e11a1492
Craig Tongue <ctongue@scottlogic.com>
2018-10-05 14:02:25 +0100

    Initial framework

---
+++ src/core/main.js
@@ -0,0 +1,10 @@
+const log = require('log');
+const utils = require('utils');
+
+module.exports.loop = function () {
+    log.tick();
+    let activeRooms = utils.getActiveRooms();
+    let currentRoom;
+
+
+};
---
+++ src/core/utils.js
@@ -0,0 +1,9 @@
+
+const utils = {
+
+    getActiveRooms : function() {
+        return Game.rooms;
+    },
+};
+
+module.exports = utils;
